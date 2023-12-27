6a83fb123a5e1b53c25874bff4ad5c5c94704b60
Michał Kurczak <kurczakmichal97@gmail.com>
2023-12-27 08:22:10 +0100

    Add QuestionListDTO class to question_dto.dart

--- lib/data/model/question_dto.dart
+++ lib/data/model/question_dto.dart
@@ -3,6 +3,18 @@ import 'package:json_annotation/json_annotation.dart';
 part 'question_dto.g.dart';
 
 @JsonSerializable()
+class QuestionListDTO {
+  final List<QuestionDTO> questions;
+
+  QuestionListDTO({required this.questions});
+
+  factory QuestionListDTO.fromJson(Map<String, dynamic> json) =>
+      _$QuestionListDTOFromJson(json);
+
+  Map<String, dynamic> toJson() => _$QuestionListDTOToJson(this);
+}
+
+@JsonSerializable()
 class QuestionDTO {
   final String question;
   final List<String> answers;
