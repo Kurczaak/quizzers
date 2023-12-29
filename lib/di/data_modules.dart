4b23e77cb025ac748f644f47cdbc86909a07703f
Michał Kurczak <kurczakmichal97@gmail.com>
2023-12-29 08:55:40 +0100

    Add API key to Dio headers

--- lib/di/data_modules.dart
+++ lib/di/data_modules.dart
@@ -1,13 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
 import 'package:quizzers/data/client/open_ai_client.dart';
 
 @module
 abstract class RetrofitInjectableModule {
+  final apiKey = dotenv.env['API_KEY'];
   @LazySingleton()
-  Dio get dio => Dio();
-  OpenAIClient getService(Dio client, @factoryParam String url) => OpenAIClient(
+  Dio get dio => Dio(
+        BaseOptions(
+          headers: {'authorization': 'Bearer $apiKey'},
+          connectTimeout: const Duration(seconds: 30), // TODO extract
+          receiveTimeout: const Duration(seconds: 30), // TODO extract
+        ),
+      );
+  OpenAIClient getService(Dio client) => OpenAIClient(
         client,
-        baseUrl: 'https://api.openai.com/',
-      ); // TODO extract to config
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:quizzers/di/injection.config.dart';
 import 'package:quizzers/di/injection.dart';
 import 'package:quizzers/home_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   getIt.init();
   runApp(const MyApp());
 }
